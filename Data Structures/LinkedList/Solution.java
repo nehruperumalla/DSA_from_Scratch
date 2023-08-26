85f9695b24b09e0a840d43103c5ca846c34b82c5
Nehru Perumalla <nehruperumalla@gmail.com>
2023-08-25 22:49:46 -0500

    LinkedList Function Calls

--- Data Structures/LinkedList/Solution.java
+++ Data Structures/LinkedList/Solution.java
@@ -1,5 +1,17 @@
 public class Solution {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
+        list.add(10);
+        System.out.println(list);
+        list.add(11);
+        System.out.println(list);
+        list.add(12);
+        System.out.println(list);
+        list.add(13);
+        System.out.println(list);
+        list.removeFirst();
+        System.out.println(list);
+        list.removeLast();
+        System.out.println(list);
     }
 }
